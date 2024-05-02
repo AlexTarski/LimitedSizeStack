8785ac373d5a18fb3f9c48e528454dcf9bd00363
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-03 00:55:51 +0300

    Add LimitedSizeStack class

--- LimitedSizeStack.cs
+++ LimitedSizeStack.cs
@@ -1,22 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LimitedSizeStack;
 
 public class LimitedSizeStack<T>
 {
+	private LinkedList<T> stack = new ();
+	private int limit;
 	public LimitedSizeStack(int undoLimit)
 	{
+		this.limit = undoLimit;
 	}
 
 	public void Push(T item)
 	{
-		throw new NotImplementedException();
+		if (stack.Count < limit)
+		{
+			stack.AddLast(item);
+		}
+		else
+		{
+			stack.AddLast(item);
+			stack.RemoveFirst();
+		}
 	}
 
 	public T Pop()
 	{
-		throw new NotImplementedException();
+		if (stack.Count == 0) throw new InvalidCastException();
+		T item = stack.Last.Value;
+		stack.RemoveLast();
+		return item;
 	}
 
-	public int Count => throw new NotImplementedException();
+	public int Count => stack.Count;
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LimitedSizeStack
+
+The "LimitedSizeStack" class implements a stack of limited size:
+it works almost like a usual stack, but when it reaches its maximum capacity,
+the deepest (first) element will be removed before adding a new one.
+
+Hence, stack always stay of limited size.
