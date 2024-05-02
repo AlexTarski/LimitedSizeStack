886eb6c800d2957df971859694483046050422b4
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-02 20:00:45 +0300

    Добавьте файлы проекта.

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+	public LimitedSizeStack(int undoLimit)
+	{
+	}
+
+	public void Push(T item)
+	{
+		throw new NotImplementedException();
+	}
+
+	public T Pop()
+	{
+		throw new NotImplementedException();
+	}
+
+	public int Count => throw new NotImplementedException();
+}
