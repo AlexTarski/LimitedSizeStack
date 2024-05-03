a0547cd23658a45546fa49bbebfba74e56865506
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-03 23:27:18 +0300

    Add Cancel button feature

--- ListModel.cs
+++ ListModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework.Interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace LimitedSizeStack;
+
+public class Operation<TItem>
+{
+	public string OperationType { get; set; }
+	public int OperationId { get; set; }
+	public TItem Item { get; set; }
+
+    public Operation(string name, int index, TItem item)
+    {
+        OperationType = name;
+		OperationId = index;
+		Item = item;
+    }
+}
 
 public class ListModel<TItem>
 {
 	public List<TItem> Items { get; }
 	public int UndoLimit;
+	public LimitedSizeStack<Operation<TItem>> History;
         
 	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
 	{
@@ -16,25 +34,42 @@ public class ListModel<TItem>
 	{
 		Items = items;
 		UndoLimit = undoLimit;
+		History = new(undoLimit);
 	}
 
 	public void AddItem(TItem item)
 	{
 		Items.Add(item);
+		Operation<TItem> operation = new("Add", Items.Count - 1, item);
+		History.Push(operation);
 	}
 
 	public void RemoveItem(int index)
 	{
-		Items.RemoveAt(index);
+        Operation<TItem> operation = new("Remove", index, Items[index]);
+		History.Push(operation);
+        Items.RemoveAt(index);
 	}
 
 	public bool CanUndo()
 	{
+		if(History.Count > 0)
+		{
+			return true;
+		}
 		return false;
 	}
 
 	public void Undo()
 	{
-		throw new NotImplementedException();
-	}
+		Operation<TItem> operation = History.Pop();
+		if(operation.OperationType == "Remove")
+		{
+			Items.Insert(operation.OperationId, operation.Item);
+		}
+        else
+        {
+			Items.RemoveAt(operation.OperationId);
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # LimitedSizeStack
 
+This project is a simple task manager: you can add or remove tasks from the list and
+undo your actions if it would be necessary.
+
+ListModel.undoLimit - count of possible undo actions.
+CanUndo() - check if it`s possible to undo the action.
+
 The "LimitedSizeStack" class implements a stack of limited size:
 it works almost like a usual stack, but when it reaches its maximum capacity,
 the deepest (first) element will be removed before adding a new one.
 
 Hence, stack always stay of limited size.
+
+LimitedSizeStack keeps the history of operations in it.
